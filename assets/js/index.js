4ee16a8a39b03a21e639975624e1a04ada99aa88
esdrasdev12 <esdras.cff@gmail.com>
2023-06-29 17:58:16 -0300

    Starting logic in Javascript

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+const buttonPlay = document.querySelector(".buttonPlay")
+const minutesDisplay = document.querySelector(".minutes")
+const secondsDisplay = document.querySelector(".seconds")
+
+initialTime
+
+buttonPlay.addEventListener('click', () => {
+  updateTimer()
+})
+
+function updateTimer () {
+  // criar lógica do update timer
+}
