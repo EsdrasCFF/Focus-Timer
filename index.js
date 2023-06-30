18045cc6a76e8beb4a2188802ca43d8db1eb4415
esdrasdev12 <esdras.cff@gmail.com>
2023-06-30 17:55:28 -0300

    Ending Alert Error CSS and HTML

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const buttonPlay = document.querySelector(".buttonPlay")
 const buttonPause = document.querySelector(".buttonPause")
 const buttonSetting = document.querySelector(".buttonSetting")
 const buttonStop = document.querySelector(".buttonStop")
+const buttonSendMinutes = document.querySelector(".buttonSend")
 
 const minutesDisplay = document.querySelector(".minutes")
 const secondsDisplay = document.querySelector(".seconds")
+const alertPromptDisplay = document.querySelector(".alert-prompt")
+const alertErrorDisplay = document.querySelector(".alert-error")
 
+
+let inputMinutes;
 let intervalId;
-let userMinutes;
+
 
 buttonPlay.addEventListener('click', () => {
   intervalId = setInterval(updateTimer, 1000)
@@ -38,8 +43,18 @@ buttonStop.addEventListener('click', () => {
 })
 
 buttonSetting.addEventListener('click', () => {
-  userMinutes = prompt("Informe a quantidade de minutos para o Timer: ")
-  minutesDisplay.textContent = userMinutes
+  alertPromptDisplay.classList.toggle('open')
+})
+
+buttonSendMinutes.addEventListener('click', () => {
+  inputMinutes = document.querySelector("#userMinutes").value
+  if(inputMinutes < 1) {
+    
+  }
+
+  //minutesDisplay.textContent = inputMinutes
+
+  //alertPromptDisplay.classList.remove("open")
 })
 
 function updateTimer () {
@@ -65,4 +80,9 @@ function updateTimer () {
     initialMinutes --
     minutesDisplay.textContent = initialMinutes.toString().padStart(2, "0")
   }
+}
+
+function checkInputValue() {
+  let inputMinutes = document.querySelector('.userMinutes').value
+  console.log(inputMinutes)
 }
