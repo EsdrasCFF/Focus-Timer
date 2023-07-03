9c68af919f2478762dd7b7b6f01ad938ca54aaaa
esdrasdev12 <esdras.cff@gmail.com>
2023-07-02 22:03:35 -0300

    Refactoring the application

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { updateTimer } from "./timer"
+
 const buttonPlay = document.querySelector(".buttonPlay")
 const buttonPause = document.querySelector(".buttonPause")
 const buttonSetting = document.querySelector(".buttonSetting")
@@ -91,7 +93,7 @@ buttonSendMinutes.addEventListener('click', () => {
   } else {
     alertErrorDisplay.classList.add("hidden")
     alertPromptDisplay.classList.remove("open")
-    minutes.textContent = inputMinutes
+    minutes.textContent = twoDigits(inputMinutes)
   }
 
 })
@@ -112,31 +114,6 @@ buttonSoundOff.addEventListener('click', () => {
 })
 
 
-function updateTimer () {
-  let initialMinutes = minutesDisplay.textContent
-  let initialSeconds = secondsDisplay.textContent
-
-  if(initialMinutes < 1 && initialSeconds < 1) {
-    clearInterval(intervalId)
-
-    buttonStop.classList.add("hidden")
-    buttonPause.classList.add("hidden")
-    buttonSetting.classList.remove("hidden")
-    buttonPlay.classList.remove("hidden")
-    
-    soundStop.play()
-    return
-  }
-
-  if(initialSeconds > 0) {
-    initialSeconds --
-    secondsDisplay.textContent = initialSeconds.toString().padStart(2, "0")
-  } else {
-    secondsDisplay.textContent = 10
-    initialMinutes --
-    minutesDisplay.textContent = initialMinutes.toString().padStart(2, "0")
-  }
-}
 
 
 function isNumber(char) {
